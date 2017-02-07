56de8ea09781a375e8c1a3589d50d448b94984c7
Nicolas S. Dade <nic.dade@gmail.com>
2017-02-06 17:24:32 -0800

    unify the docs

---
+++ docs.go
@@ -0,0 +1,16 @@
+/*
+  Tiny package to define two compile-time constants
+    Big
+	Little
+  one of which is true and the other false, depending on
+  what the endiannesss of the CPU is.
+
+  This information is crucial in bit hacks, yet is lacking
+  as a constant.
+
+  Two constants is redundant, but having both makes the using code
+  easier to read because it avoids negations in the if conditions.
+
+  Copyright 2017 Nicolas S. Dade
+*/
+package cpuendian
