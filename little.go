f12f5efff34f8522ea3c01d964d2a24120630cbf
Nicolas S. Dade <nic.dade@gmail.com>
2022-06-20 22:06:09 -0700

    update list of little-endian architectures: add riscv64
    and note that amd64p32 was removed from Go in go 1.14 in 2019.
    
    Since with go modules no one using amd64p32 should pull in the
    newer cpuendian package, I can remove it now.

--- little.go
+++ little.go
@@ -1,7 +1,8 @@
-//go:build 386 || amd64 || amd64p32 || arm || arm64 || ppc64le || mips64le || mipsle || wasm
-// +build 386 amd64 amd64p32 arm arm64 ppc64le mips64le mipsle wasm
+//go:build 386 || amd64 || arm || arm64 || ppc64le || mips64le || mipsle || riscv64 || wasm
+// +build 386 amd64 arm arm64 ppc64le mips64le mipsle riscv64 wasm
 
 // note list of little-endian GOARCH comes from <go compiler>/src/syscall/endian_little.go
+// note 2: the little-endian architecture 'amd64p32' was removed from the compiler in go 1.14.
 
 package cpuendian
 
